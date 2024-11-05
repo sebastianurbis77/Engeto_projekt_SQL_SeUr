007190f50497391dbbc0839027d409ac9e6a5658
sebastianurbis77 <160941121+sebastianurbis77@users.noreply.github.com>
2024-11-05 22:35:21 +0100

    Update and rename Projekt_SQL_SeUr.sql to Projekt_SQL_SeUr_tables.sql

--- Projekt_SQL_SeUr_tables.sql
+++ Projekt_SQL_SeUr_tables.sql
@@ -20,6 +20,10 @@ GROUP BY YEAR (date_from);
 
 	-- z dostupných informací jsme zjistili, že společné porovnatelné období jsou roky 2006-2018
 
+/*
+ * Table t_sebastian_urbis_project_SQL_primary_final	*1
+ */
+
 CREATE OR REPLACE VIEW t_sebastian_urbis_project_SQL_primary_final AS
 SELECT
 	cpc.name AS food_item,
@@ -41,3 +45,25 @@ JOIN czechia_payroll_industry_branch cpib
 	ON cp2.industry_branch_code = cpib.code;
 
  -- pojítko pro naše tabulky byl společný rok, díky tomu jsme si mohli vypsat všechna potřebná data pro anlýzu výzkumných otázek
+
+
+/*
+ * Table t_sebastian_urbis_project_SQL_secondary_final		*2
+ */
+
+CREATE OR REPLACE VIEW t_sebastian_urbis_project_SQL_secondary_final AS
+SELECT
+	c.country,
+	c.capital_city,
+	c.currency_code,
+	c.population,
+	c.region_in_world,
+	e.`year`,
+	e.GDP 
+FROM countries c
+JOIN economies e 
+	ON c.country = e.country
+		WHERE c.continent = 'Europe'
+			AND e.`year` BETWEEN 2006 AND 2018; 
+
+ -- na základě zadání jsme se soustředili na Evropské státy, kde je stěžejní údaj HDP
